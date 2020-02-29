e9264f49ecb4f3a67495622d07f3d27d5c4c2f04
mo <thinkgo@aliyun.com>
2020-02-29 20:50:04 +0800

    update builder

--- _example/builder/main.go
+++ _example/builder/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	fmt.Println("Build Time: " + builder.BuildTime)
 	fmt.Println("Git Commit: " + builder.GitCommit)
+	fmt.Println("Git Full Commit: " + builder.GitFullCommit)
 	fmt.Println("Version: " + builder.Version)
 	fmt.Println("API Version: " + builder.APIVersion)
 	fmt.Println("Model: " + builder.Model)
--- builder/builder.go
+++ builder/builder.go
@@ -3,8 +3,10 @@ package builder
 var (
 	// BuildDate 编译日期 由外部ldflags指定
 	BuildTime = ""
-	// GitCommit git提交版本 由外部ldflags指定
+	// GitCommit git提交版本(短) 由外部ldflags指定
 	GitCommit = ""
+	// GitFullCommit git提交版本(完整) 由外部ldflags指定
+	GitFullCommit = ""
 	// Version 版本 由外部ldflags指定
 	Version = "v0.0.1 Beta"
 	// APIVersion api版本 由外部ldflags指定
