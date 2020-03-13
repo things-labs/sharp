853bc4b2df823ee1aca55e72412c1be4d1ec9fbe
mo <thinkgo@aliyun.com>
2020-03-13 12:04:43 +0800

    修改名称 改成和uber一致

--- errors.go
+++ errors.go
@@ -7,25 +7,20 @@ import (
 // Errors contains all happened errors
 type Errors []error
 
-// HasError return whether any error in Errors
-func (sf Errors) HasError() bool {
-	return len(sf) > 0
-}
-
 // GetErrors gets all errors that have occurred and returns a slice of errors (Error type)
-func (errs Errors) GetErrors() []error {
+func (errs Errors) Errors() []error {
 	return errs
 }
 
 // Add adds an error to a given slice of errors
-func (sf Errors) Add(newErrors ...error) Errors {
+func (sf Errors) Combine(newErrors ...error) Errors {
 	for _, err := range newErrors {
 		if err == nil {
 			continue
 		}
 
 		if errors, ok := err.(Errors); ok {
-			sf = sf.Add(errors...)
+			sf = sf.Combine(errors...)
 		} else {
 			ok = true
 			for _, e := range sf {
--- errors_test.go
+++ errors_test.go
@@ -9,17 +9,17 @@ func TestErrorsCanBeUsedOutsideGorm(t *testing.T) {
 	errs := []error{errors.New("First"), errors.New("Second")}
 
 	gErrs := Errors(errs)
-	gErrs = gErrs.Add(nil)
-	gErrs = gErrs.Add(errors.New("Third"))
-	gErrs = gErrs.Add(gErrs)
+	gErrs = gErrs.Combine(nil)
+	gErrs = gErrs.Combine(errors.New("Third"))
+	gErrs = gErrs.Combine(gErrs)
 
 	if gErrs.Error() != "First; Second; Third" {
 		t.Fatalf("Gave wrong error, got %s", gErrs.Error())
 	}
-	if length := len(gErrs.GetErrors()); length != 3 {
+	if length := len(gErrs.Errors()); length != 3 {
 		t.Fatalf("Length of errors, got %+v", length)
 	}
-	if !gErrs.HasError() {
-		t.Fatalf("Has wrong error in errors, got %+v", gErrs.HasError())
+	if len(gErrs.Errors()) == 0 {
+		t.Fatalf("Has wrong error in errors, got %+v", gErrs.Errors())
 	}
 }
