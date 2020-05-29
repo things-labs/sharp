a2659d0a1b8397112f88d2b0b90bd5c915210357
mo <thinkgo@aliyun.com>
2020-05-29 15:22:08 +0800

    增加相关error

--- iorm/errors.go
+++ iorm/errors.go
@@ -4,8 +4,13 @@ import (
 	"errors"
 )
 
+// 基本错误
 var (
-	ErrZeroOrEmptyValue   = errors.New("value must not be zero or empty")
-	ErrObjectAlreadyExist = errors.New("object already exist")
-	ErrHasAssociation     = errors.New("object has associate other object")
+	ErrZeroOrEmptyValue    = errors.New("value must not be zero or empty")
+	ErrObjectAlreadyExist  = errors.New("object already exist")
+	ErrHasAssociation      = errors.New("object has associate other object")
+	ErrOperateNotPermitted = errors.New("operate not permitted")
+	ErrOutOfRange          = errors.New("out of range")
+	ErrInvalidParameter    = errors.New("invalid parameter")
+	ErrConflict            = errors.New("data field conflict")
 )
