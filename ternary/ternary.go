dadc86f5ef5e23a4b03e18ec35b3c3a3dd2cef40
mo <thinkgo@aliyun.com>
2020-06-19 16:55:05 +0800

    增加三目运算

---
+++ ternary/ternary.go
@@ -0,0 +1,29 @@
+package ternary
+
+func If(condition bool, trueVal, falseVal interface{}) interface{} {
+	if condition {
+		return trueVal
+	}
+	return falseVal
+}
+
+func IfInt(condition bool, trueVal, falseVal int64) int64 {
+	if condition {
+		return trueVal
+	}
+	return falseVal
+}
+
+func IfFloat(condition bool, trueVal, falseVal float64) float64 {
+	if condition {
+		return trueVal
+	}
+	return falseVal
+}
+
+func IfString(condition bool, trueVal, falseVal string) string {
+	if condition {
+		return trueVal
+	}
+	return falseVal
+}
