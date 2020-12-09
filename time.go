93621a87577b5f3a01fd7effd77ae00f8c77b6c0
mo <thinkgo@aliyun.com>
2020-12-09 11:49:32 +0800

    add

---
+++ time.go
@@ -0,0 +1,25 @@
+package sharp
+
+import (
+	"fmt"
+	"time"
+)
+
+func TimeAgo(t time.Time) string {
+	now := time.Now()
+	diff := now.Sub(t)
+	hours := diff.Hours()
+	switch {
+	case hours < 1.0:
+		return fmt.Sprintf("约 %.0f 分钟前", diff.Minutes())
+	case hours < 24.0:
+		return fmt.Sprintf("约 %.0f 小时前", hours)
+	case hours < 72.0:
+		return fmt.Sprintf("约 %.0f 天前", hours/24.0)
+	default:
+		if now.Year() == t.Year() { // 同一年，不用年份
+			return t.Format("01-02 15:04")
+		}
+		return t.Format("2006-01-02")
+	}
+}
---
+++ time_test.go
@@ -0,0 +1,50 @@
+package sharp
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAgo(t *testing.T) {
+	type args struct {
+		t time.Time
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"小于1分钟",
+			args{time.Now().Add(-30 * time.Second)},
+			"约 1 分钟前",
+		},
+		{
+			"约2小时前",
+			args{time.Now().Add(-100 * time.Minute)},
+			"约 2 小时前",
+		},
+		{
+			"约1天前",
+			args{time.Now().Add(-25 * time.Hour)},
+			"约 1 天前",
+		},
+		{
+			"11-02 14:00",
+			args{time.Date(time.Now().Year(), 11, 2, 14, 0, 0, 0, time.Local)},
+			"11-02 14:00",
+		},
+		{
+			"2016-02-02",
+			args{time.Date(2016, 2, 2, 0, 0, 0, 0, time.Local)},
+			"2016-02-02",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TimeAgo(tt.args.t); got != tt.want {
+				t.Errorf("Ago() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
