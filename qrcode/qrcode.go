c8882cb894ff6656ced680d0fb2d54169d6a480f
thinkgo <thinkgo@aliyun.com>
2021-01-07 10:26:54 +0800

    rename qrcode api

--- qrcode/qrcode.go
+++ qrcode/qrcode.go
@@ -7,7 +7,8 @@ import (
 	"image/gif"
 	"image/jpeg"
 	"image/png"
-	"io/ioutil"
+	"io"
+	"os"
 	"path/filepath"
 	"strings"
 
@@ -37,8 +38,8 @@ type MetaInfo struct {
 	Ext     string // 命名扩展名
 }
 
-// Generate generate to barcode.Barcode
-func (sf *MetaInfo) Generate() (barcode.Barcode, error) {
+// Encode encode to barcode.Barcode
+func (sf *MetaInfo) Encode() (barcode.Barcode, error) {
 	code, err := qr.Encode(sf.Content, sf.Level, sf.Mode)
 	if err != nil {
 		return nil, err
@@ -46,64 +47,75 @@ func (sf *MetaInfo) Generate() (barcode.Barcode, error) {
 	return barcode.Scale(code, sf.Width, sf.Height)
 }
 
-// GenerateToBytes generate to byte
-func (sf *MetaInfo) GenerateToBytes() ([]byte, string, error) {
-	if !extstr.ContainsFold([]string{ExtJPG, ExtPNG, ExtGIF, ExtBMP}, sf.Ext) {
-		return nil, "", fmt.Errorf("not support image format: %s", sf.Ext)
-	}
-
-	code, err := sf.Generate()
-	if err != nil {
-		return nil, "", err
-	}
-
-	buf := &bytes.Buffer{}
-	switch strings.ToLower(sf.Ext) {
-	case ExtJPG:
-		err = jpeg.Encode(buf, code, nil)
-	case ExtPNG:
-		err = png.Encode(buf, code)
-	case ExtGIF:
-		err = gif.Encode(buf, code, nil)
-	case ExtBMP:
-		err = bmp.Encode(buf, code)
-	}
-	if err != nil {
-		return nil, "", err
-	}
-	return buf.Bytes(), algo.SHA1(sf.Content) + sf.Ext, nil
+// Filename filename
+func (sf *MetaInfo) Filename() string {
+	return algo.SHA1(sf.Content) + sf.Ext
 }
 
-// GenerateToFile generate QR code, return filename
-func (sf *MetaInfo) GenerateToFile(path string) (string, error) {
-	data, filename, err := sf.GenerateToBytes()
+// Write wirte QR code
+func (sf *MetaInfo) Write(w io.Writer) error {
+	if !extstr.ContainsFold([]string{ExtJPG, ExtPNG, ExtGIF, ExtBMP}, sf.Ext) {
+		return fmt.Errorf("not support image format: %s", sf.Ext)
+	}
+
+	code, err := sf.Encode()
+	if err != nil {
+		return err
+	}
+	switch strings.ToLower(sf.Ext) {
+	case ExtJPG:
+		err = jpeg.Encode(w, code, nil)
+	case ExtPNG:
+		err = png.Encode(w, code)
+	case ExtGIF:
+		err = gif.Encode(w, code, nil)
+	case ExtBMP:
+		err = bmp.Encode(w, code)
+	}
+	return err
+}
+
+// WriteFile write QR code to file, return filename
+func (sf *MetaInfo) WriteFile(path string) (string, error) {
+	filename := sf.Filename()
+	dstFilename := filepath.Join(path, filename)
+	if extos.IsExist(dstFilename) {
+		return filename, nil
+	}
+
+	f, err := os.OpenFile(dstFilename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
 	if err != nil {
 		return "", err
 	}
-	dst := filepath.Join(path, filename)
-	if extos.IsExist(dst) {
-		return filename, nil
-	}
-	return filename, ioutil.WriteFile(dst, data, 0664)
+	defer f.Close()
+	return filename, sf.Write(f)
 }
 
-// GenerateToImageB64 generate QR code to base images string, return base64 image string and filename
+// ToBytes generate to byte
+func (sf *MetaInfo) ToBytes() ([]byte, error) {
+	buf := &bytes.Buffer{}
+	if err := sf.Write(buf); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// ToBase64 generate QR code to base64 string, return base64 string
+func (sf *MetaInfo) ToBase64() (string, error) {
+	data, err := sf.ToBytes()
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(data), nil
+}
+
+// ToImageB64 generate QR code to base images string, return base64 image string
 // base64 images string format like:
 // data: image/png;base64,xxxxxxxxxx
-func (sf *MetaInfo) GenerateToImageB64() (string, string, error) {
-	b64, filename, err := sf.GenerateToBase64()
+func (sf *MetaInfo) ToImageB64() (string, error) {
+	b64, err := sf.ToBase64()
 	if err != nil {
-		return "", "", err
+		return "", err
 	}
-	return fmt.Sprintf("data:image/%s;base64,%s",
-		strings.TrimLeft(sf.Ext, "."), b64), filename, nil
-}
-
-// GenerateToBase64 generate QR code to base64 string, return base64 string and filename
-func (sf MetaInfo) GenerateToBase64() (string, string, error) {
-	data, filename, err := sf.GenerateToBytes()
-	if err != nil {
-		return "", "", err
-	}
-	return base64.StdEncoding.EncodeToString(data), filename, nil
+	return fmt.Sprintf("data:image/%s;base64,%s", strings.TrimLeft(sf.Ext, "."), b64), nil
 }
